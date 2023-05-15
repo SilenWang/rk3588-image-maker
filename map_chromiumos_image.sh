4896e0d278d129b6e100af6ece471dc5a7e5f773
Su Yue <glass@fydeos.io>
2023-05-15 18:17:01 +0800

    Update for khadas edge2
    Signed-off-by: Su Yue <glass@fydeos.io>

--- map_chromiumos_image.sh
+++ map_chromiumos_image.sh
@@ -3,14 +3,23 @@ top=$(dirname $0)
 pushd $top 2>&1 1>/dev/null
 top=$(pwd)
 img_dir="$top/Image"
-chromeos_image=$1
+chromeos_image=""
 loopdev=$(sudo losetup -f)
 newest_loader="v1.07.111"
+default_uboot="uboot.img"
 get_miniloader=false
+board=""
 
 declare -A LINK_MAP=(
   [inaugural]="v1.04.106"
   [fydetab_duo]="v1.06.111"
+  [khadas-edge2]="v1.09.111"
+)
+
+declare -A UBOOT_MAP=(
+  [inaugural]="uboot.img"
+  [fydetab_duo]="uboot.img"
+  [khadas-edge2]="edge2-uboot.img"
 )
 
 die() {
@@ -18,11 +27,26 @@ die() {
   exit 1
 }
 
-[ -b "$loopdev" ] || die "no loop device found."
+parse_options()
+{
+  while [ "$1" ]; do
+    case "$1" in
+      "--board"|"-b")
+        board=$2
+        [ -z ${LINK_MAP[$board]} ] && die "invalid board $board"
+        shift
+      ;;
+    *)
+      chromeos_image=$1
+      ;;
+    esac
+    shift
+  done
 
-if [ ! -f "$chromeos_image" ]; then
-  die "Usage:$0 [chromiumos_image bin]"
-fi
+  [ ! -f "$chromeos_image" ] && die "Usage:$0 [chromiumos_image bin]"
+}
+
+[ -b "$loopdev" ] || die "no loop device found."
 
 mount_loopdev() {
   local img=$1
@@ -42,22 +66,44 @@ link_img() {
 }
 
 link_miniloader() {
-  for board in "${!LINK_MAP[@]}"; do
-    echo "check board:$board"
-    if [ -n "$(echo $chromeos_image | grep $board)" ]; then
-      echo "link miniloader: ${LINK_MAP[$board]}"
-      ln -sf $top/rk3588-uboot-bin/rk3588_spl_loader_${LINK_MAP["$board"]}.bin $img_dir/MiniLoaderAll.bin
-      get_miniloader=true
-      break
+  for b in "${!LINK_MAP[@]}"; do
+    [ -n $board ] && break;
+    echo "check board:$b"
+
+    if [ -n "$(echo $chromeos_image | grep $b)" ]; then
+        board=$b
     fi
   done
+
+  if [ -n "$board" ]; then
+    echo "link miniloader: ${LINK_MAP[$board]}"
+    ln -sf $top/rk3588-uboot-bin/rk3588_spl_loader_${LINK_MAP["$board"]}.bin $img_dir/MiniLoaderAll.bin
+    get_miniloader=true
+  fi
+
   if ! $get_miniloader; then
     echo "Use the newset loader: $newest_loader"
     ln -sf $top/rk3588-uboot-bin/rk3588_spl_loader_${newest_loader}.bin $img_dir/MiniLoaderAll.bin
   fi
 }
 
+link_uboot() {
+  local get_uboot=false
+
+  if [ -n "$board" ]; then
+    echo "link uboot.img: ${UBOOT_MAP[$board]}"
+    ln -sf $top/rk3588-uboot-bin/${UBOOT_MAP["$board"]} ${img_dir}/uboot.img
+    get_uboot=true
+  fi
+
+  if ! $get_uboot; then
+    echo "Use the default uboot: $default_uboot"
+    ln -sf $top/rk3588-uboot-bin/${default_uboot} ${img_dir}/uboot.img
+  fi
+}
+
 main() {
+  parse_options "$@"
   mount_loopdev $chromeos_image
   link_img p1 STATE
   link_img p2 KERN-A
@@ -65,7 +111,8 @@ main() {
   link_img p8 OEM
   link_img p12 EFI-SYSTEM
   link_miniloader
+  link_uboot
 }
 
-main
+main "$@"
 popd 2>&1 1>/dev/null
